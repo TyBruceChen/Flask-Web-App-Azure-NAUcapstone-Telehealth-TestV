8b8e5350d171d89a1c05166ee25e67286e6975f6
Tianyi (Bruce) Chen <152252677+TyBruceChen@users.noreply.github.com>
2024-02-16 15:48:52 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask,url_for,render_template,request
+from PIL import Image
+import os
+import time
+
+
+
+app = Flask(__name__)
+"""
+@app.route('/hello/')    #returns a response once URL/hello is requested
+def hello_world():
+#    print(verbose)
+    return render_template('index.html',name = None)
+"""
+
+@app.route('/temp/<var>')
+def temp(var):
+    return var
+#url_for('hello_world',verbose = 1)
+@app.route('/hello/',methods = ['GET','POST'])
+def file_handle():
+    file_name = 'downloads/temp.png'
+    wait_time = 3  #let the thread to run 10s
+    if request.method == 'POST':
+        print(request.files)
+        if 'pic' in request.files:
+            f = request.files['pic']
+            f.save(file_name)
+            img = Image.open(file_name)
+            img.show()
+            start_time = time.time()
+            while start_time-time.time() <= wait_time:
+                continue
+    return render_template('/index.html', name = None)
+
+'''
+with app.test_request_context():
+    print(url_for('temp',var='Hello'))
+    url_for('static',filename = 'files/Bart-Philip-me.png')
+'''
+app.run(host = '0.0.0.0',port = os.getenv('PORT',80))
+
+
+
+
+"""
+def create_app(test_config = None):
+    app = Flask(__name__,instance_relative_config = True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path,'Flask.sqlite'),
+    )
+
+    if test_config is None:
+        #load the instance config
+        app.config.from_pyfile('config.py',silent = True)
+    else:
+        #load the test config
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    @app.route('/hello')    #handle response to URL/hello
+    def hello():
+        return 'Hello, World!'
+    
+    return app
+"""
