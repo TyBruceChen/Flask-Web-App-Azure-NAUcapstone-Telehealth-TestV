38709d134e23aa397d2e6c88667c848e0fb22b5b
Tianyi (Bruce) Chen <152252677+TyBruceChen@users.noreply.github.com>
2024-02-17 13:56:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import time
 import random
 
 
-temp_img_path = 'temp_imgs'
+temp_img_path = '/home/site/temp_imgs'
 app = Flask(__name__)
 if os.path.exists(temp_img_path) != True:
         os.mkdir(temp_img_path)
